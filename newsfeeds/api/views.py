72e027bbe0a994118a6d2408e20fcec1e146d5ff
Yi Wang <75833665+yiwang2465@users.noreply.github.com>
2021-06-26 22:59:05 -0700

    add newsfeeds api and unit tests

---
+++ newsfeeds/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from newsfeeds.models import NewsFeed
+from newsfeeds.api.serializers import NewsFeedSerializer
+
+
+class NewsFeedViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # define queryset as the user needs to be authenticated to see newsfeeds
+        # self.request.user.newsfeed_set.all()
+        # NewsFeed.objects.filter
+        return NewsFeed.objects.filter(user=self.request.user)
+
+    def list(self, request):
+        serializer = NewsFeedSerializer(self.get_queryset(), many=True)
+        return Response({
+            'newsfeeds': serializer.data,
+        }, status=status.HTTP_200_OK)
---
+++ newsfeeds/services.py
@@ -0,0 +1,22 @@
+from newsfeeds.models import NewsFeed
+from friendships.services import FriendshipService
+
+
+class NewsFeedService(object):
+
+    @classmethod
+    def fanout_to_followers(cls, tweet):
+        # Do not use N+1 query
+        # for follower in FriendshipService.get_followers(tweet.user):
+        #     NewsFeed.objects.create(
+        #         user=follower,
+        #         tweet=tweet,
+        #     )
+
+        # Use bulk_create, all inserts will be a single operation
+        newsfeeds = [
+            NewsFeed(user=follower, tweet=tweet)
+            for follower in FriendshipService.get_followers(tweet.user)
+        ]
+        newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
+        NewsFeed.objects.bulk_create(newsfeeds)
