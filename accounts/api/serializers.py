57d21fb6b831332857b28888285ed97ab8dacf79
Yi Wang <75833665+yiwang2465@users.noreply.github.com>
2021-07-01 22:08:23 -0700

    add comment list api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -18,6 +18,10 @@ class UserSerializerForFriendship(UserSerializerForTweet):
     pass
 
 
+class UserSerializerForComment(UserSerializerForTweet):
+    pass
+
+
 class SignupSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=20, min_length=6)
     password = serializers.CharField(max_length=20, min_length=6)
