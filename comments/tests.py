e69c77d3f73f3a9d1f72617041e832d743b7bad0
Yi Wang <75833665+yiwang2465@users.noreply.github.com>
2021-07-02 21:42:20 -0700

    add likes model and admin

--- comments/tests.py
+++ comments/tests.py
@@ -2,9 +2,21 @@ from testing.testcases import TestCase
 
 
 class CommentModelTest(TestCase):
+    def setUp(self):
+        self.user1 = self.create_user('user1')
+        self.tweet1 = self.create_tweet(self.user1, 'come tweet')
+        self.comment1 = self.create_comment(self.user1, self.tweet1, 'good one')
 
     def test_comment(self):
-        user = self.create_user('user1')
-        tweet = self.create_tweet(user)
-        comment = self.create_comment(user, tweet)
-        self.assertNotEqual(comment.__str__, None)
+        self.assertNotEqual(self.comment1.__str__, None)
+
+    def test_like_set(self):
+        self.create_like(self.user1, self.comment1)
+        self.assertEqual(self.comment1.like_set.count(), 1)
+
+        self.create_like(self.user1, self.comment1)
+        self.assertEqual(self.comment1.like_set.count(), 1)
+
+        user2 = self.create_user('user2')
+        self.create_like(user2, self.comment1)
+        self.assertEqual(self.comment1.like_set.count(), 2)
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,15 +1,25 @@
-from django.contrib.auth.models import User
-from django.test import TestCase
-from tweets.models import Tweet
 from datetime import timedelta
+from testing.testcases import TestCase
 from utils.time_helpers import utc_now
 
 
 class TweetTests(TestCase):
+    def setUp(self):
+        self.user1 = self.create_user('user1')
+        self.tweet1 = self.create_tweet(self.user1, content='new tweet')
 
     def test_hours_to_now(self):
-        newuser = User.objects.create_user(username='newuser')
-        tweet = Tweet.objects.create(user=newuser, content='newuser tweets!')
-        tweet.created_at = utc_now() - timedelta(hours=10)
-        tweet.save()
-        self.assertEqual(tweet.hours_to_now, 10)
+        self.tweet1.created_at = utc_now() - timedelta(hours=10)
+        self.tweet1.save()
+        self.assertEqual(self.tweet1.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(user=self.user1, target=self.tweet1)
+        self.assertEqual(self.tweet1.like_set.count(), 1)
+
+        self.create_like(self.user1, self.tweet1)
+        self.assertEqual(self.tweet1.like_set.count(), 1)
+
+        user2 = self.create_user('user2')
+        self.create_like(user2, self.tweet1)
+        self.assertEqual(self.tweet1.like_set.count(), 2)
